9996c94f890e25be8dd2d5ae5e9fd642fcb32131
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2021-11-18 19:56:10 +0900

    update

---
+++ kill.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ $(ps | grep roscore) ]]; then
+    killall rosmaster
+fi
+
+if [[ $(ps | grep momo_x64) ]]; then
+    killall momo_x64
+fi
+
+if [[ $(ps | grep momo_arm64) ]]; then
+    killall momo_arm64
+fi
+
+if [[ $(ps | grep socat) ]]; then
+    killall socat
+fi
+
+if [[ $(ps | grep accel_cmd) ]]; then
+    killall accel_cmd
+fi
+
+if [[ $(ps | grep cmder.run) ]]; then
+    killall cmder.run
+fi
