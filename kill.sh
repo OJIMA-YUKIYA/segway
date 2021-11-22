a77b60d73cc051c6a67feb30ca5c0de5a848e1c9
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2021-11-22 19:25:53 +0900

    update

--- kill.sh
+++ kill.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [[ $(ps | grep roscore) ]]; then
-    killall rosmaster
-fi
-
 if [[ $(ps | grep momo_x64) ]]; then
     killall momo_x64
 fi
@@ -22,4 +18,12 @@ fi
 
 if [[ $(ps | grep accel_cmd) ]]; then
     killall accel_cmd
+fi
+
+if [[ $(ps | grep roscore) ]]; then
+    killall rosmaster
+fi
+
+if [[ $(ps | grep rostopic) ]]; then
+    killall rostopic
 fi
