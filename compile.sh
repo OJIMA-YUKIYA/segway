e7086a255dc483195b1bc766e1e81fbeb2182d8a
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2021-12-08 19:34:06 +0900

    update

--- compile.sh
+++ compile.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-cd libsegwayrmp && make
+
+cd libsegwayrmp
+if [ ! -e lib ]; then
+    mkdir lib
+fi
+if [ $USER = 'tristar' ]; then
+    cp ./ftd2xx/linux/arm64/libftd2xx.a ./lib/
+elif [ $USER = 'ojima' ]; then
+    cp ./ftd2xx/linux/x64/libftd2xx.a ./lib/
+fi
+
+
+make
 cd .. && catkin_make
