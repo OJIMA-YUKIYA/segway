876b19a8739753e96b91b128b4621a32bc44be55
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2021-12-09 19:42:03 +0900

    update

--- src/segway_rmp/src/segway_rmp_node.cpp
+++ src/segway_rmp/src/segway_rmp_node.cpp
@@ -839,6 +839,7 @@ public:
         if (this->connected) {
             ROS_INFO("%lf, %lf", msg.axes[3] * 0.2, msg.axes[0] * 0.5);
             this->segway_rmp->move(msg.axes[3] * 0.2, msg.axes[0] * 0.5);
+            ros::Duration(0.05).sleep();
         }
         return;
     }
