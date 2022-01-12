0c9a1486b1820f94a4fc8d76a29778c209823c41
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2022-01-13 03:47:58 +0900

    update

--- src/segway_rmp/src/segway_rmp_node.cpp
+++ src/segway_rmp/src/segway_rmp_node.cpp
@@ -651,7 +651,9 @@ public:
             }
             else if (this->latch == 2) {
                 la = this->ba->controller();
-                this->lin = this->gain*la.linear_vel;
+                // this->lin = this->gain*la.linear_vel;
+                this->lin = (this->before_target_linear_vel - this->linear_vel_feedback)*0.8 + la.linear_vel;
+                this->before_target_linear_vel = this->lin;
                 this->ang = la.angular_vel;
             }
 
