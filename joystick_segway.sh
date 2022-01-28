4873d497d8e939d57318e521d2e7233149c4a5dd
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2022-01-28 16:35:21 +0900

    update

--- joystick_segway.sh
+++ joystick_segway.sh
@@ -3,7 +3,7 @@ cd ~/segway
 source /home/tristar/segway/devel/setup.bash
 # ./sora_arm64.run
 #/usr/bin/chromium-browser http://127.0.0.1:8080/html/test.html &
-./momo_x64.run --no-video-device
+./momo_arm64.run --no-video-device
 source ./devel/setup.bash
 /opt/ros/melodic/bin/roscore &
 /opt/ros/melodic/bin/rosrun joy joy_node &
