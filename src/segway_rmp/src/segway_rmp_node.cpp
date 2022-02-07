d86161735dd7a66e6435ee6e42f21e93cad6c1d5
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2022-02-07 21:02:17 +0900

    update

--- src/segway_rmp/src/segway_rmp_node.cpp
+++ src/segway_rmp/src/segway_rmp_node.cpp
@@ -623,25 +623,25 @@ public:
                 else if (!this->segway_rmp->no_data_from_segway && this->no_data_from_segway) {
                     this->segway_rmp->setMaxVelocityScaleFactor(1.0);
                     ROS_INFO("setMaxVelocityScaleFactor");
-                    ros::Duration(0.05).sleep();
+                    ros::Duration(0.1).sleep();
                     this->segway_rmp->setMaxAccelerationScaleFactor(1.0);
                     ROS_INFO("setMaxAccelerationScaleFactor");
-                    ros::Duration(0.05).sleep();
+                    ros::Duration(0.1).sleep();
                     this->segway_rmp->setMaxTurnScaleFactor(1.0);
                     ROS_INFO("setMaxTurnScaleFactor");
-                    ros::Duration(0.05).sleep();
+                    ros::Duration(0.1).sleep();
                     this->segway_rmp->setCurrentLimitScaleFactor(1.0);
                     ROS_INFO("setCurrentLimitScaleFactor");
-                    ros::Duration(0.05).sleep();
+                    ros::Duration(0.1).sleep();
                     this->segway_rmp->setBalanceModeLocking(true);
                     ROS_INFO("setBalanceModeLocking");
-                    ros::Duration(0.05).sleep();
+                    ros::Duration(0.1).sleep();
                     this->segway_rmp->setOperationalMode(segwayrmp::tractor);
                     ROS_INFO("setOperationalMode");
-                    ros::Duration(0.05).sleep();
+                    ros::Duration(0.1).sleep();
                     this->segway_rmp->setControllerGainSchedule(segwayrmp::heavy);
                     ROS_INFO("setControllerGainSchedule");
-                    ros::Duration(0.05).sleep();
+                    ros::Duration(0.1).sleep();
                     this->no_data_from_segway = false;
                 }
 
