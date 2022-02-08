e44d1d2c1e8415151b9fcb31e2ef7367f1efabbc
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2022-02-09 04:42:33 +0900

    update

--- webrtc_momo/html/js/webrtc.js
+++ webrtc_momo/html/js/webrtc.js
@@ -535,6 +535,7 @@ window.addEventListener("gamepadconnected", function(e) {
     console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
     e.gamepad.index, e.gamepad.id,
     e.gamepad.buttons.length, e.gamepad.axes.length);
+    
     setInterval(gameLoop, 1000.0/500.0);
     gameLoop();
 });
